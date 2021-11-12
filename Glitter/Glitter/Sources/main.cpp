6e8164743be8c89b0282bee0d4f9ffd3732528a6
weiwu3 <92898523+weiwu3@users.noreply.github.com>
2021-11-12 17:17:15 -0500

    add sound

--- Glitter/Glitter/Sources/main.cpp
+++ Glitter/Glitter/Sources/main.cpp
@@ -154,6 +154,18 @@ int main()
     // -----------------------------
     glEnable(GL_DEPTH_TEST);
 
+
+    YsSoundPlayer player1;
+    YsSoundPlayer::SoundData myWav1;
+
+    // store the filename of music
+    string fileNames[] = { "UAV1.wav", "UAV2.wav" };
+
+    // load user choice, note use of .c_str()
+    if (YSOK == myWav1.LoadWav(fileNames[1].c_str())) {
+        player1.Start();
+        player1.PlayBackground(myWav1);
+    }
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window))
