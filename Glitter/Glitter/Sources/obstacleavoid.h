7933b64cf50ff604033b6ec6592c648f4e65b98a
Shaobow <94007723+shaobow@users.noreply.github.com>
2021-11-30 18:11:39 -0500

    curr

--- Glitter/Glitter/Sources/obstacleavoid.h
+++ Glitter/Glitter/Sources/obstacleavoid.h
@@ -12,18 +12,18 @@ public:
 
 };
 
-class detector {
-private:
-	float tolerance;
-
-	uav myUAV;
-	Map myCity;
-	glm::vec3 getForwardDir(obstacle* obst, uav* craft);
-	
-public:
-	detector(Map& city, uav& drone);
-
-	// check if an obstacle should be in map 
-	bool isInMap(obstacle obst, std::vector<obstacle> list, glm::vec3 fwdDir);
-	glm::vec3 getRepulse(obstacle* obst, uav* craft);
-};
+//class detector {
+//private:
+//	float tolerance;
+//
+//	uav myUAV;
+//	Map myCity;
+//	glm::vec3 getForwardDir(obstacle* obst, uav* craft);
+//	
+//public:
+//	detector(Map& city, uav& drone);
+//
+//	// check if an obstacle should be in map 
+//	bool isInMap(obstacle obst, std::vector<obstacle> list, glm::vec3 fwdDir);
+//	glm::vec3 getRepulse(obstacle* obst, uav* craft);
+//};
